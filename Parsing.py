6ce8c0742e21ab47459ad15d5565ba1523a9702a
JHeczko <jakub.heczko5@gmail.com>
2025-02-12 23:53:25 +0100

    minor parsing changes

--- Parsing.py
+++ Parsing.py
@@ -2,7 +2,25 @@ from openpyxl import Workbook,load_workbook
 from openpyxl.styles import Font, Alignment, PatternFill
 
 
-def edit_excel(workbook,i_mama, i_mail, i_r1, i_r2, i_r3):
+def edit_excel(workbook,i_mama, i_mail, i_r1, i_r2, i_r3, i_r4=None, i_r5 = None, i_r6=None, mode=0):
+    '''
+    basicly a heart of a program which preprocess a excel files(processing logic here). Also it can work in 3 modes:
+
+    * NAME SURNAME: first name and then surname
+    * SURNAME NAME: first surname and then name
+
+    :param workbook:
+    :param i_mama: index of name and surname of Mum inside a excel table
+    :param i_mail: index of mail
+    :param i_r1: index of first interval of payment
+    :param i_r2: index of second interval of payment
+    :param i_r3: index of third interval of payment
+    :param i_r4: index of fourth interval of payment
+    :param i_r5: index of fifth interval of payment
+    :param i_r6: index of sixth interval of payment
+    :param mode: how to process names and surnames, explained at the top
+    :return: processed and nicely colored workbook
+    '''
     newWorkBook = Workbook()
     newSheet = newWorkBook.active
     sheet = workbook.active  # Pobieramy aktywny arkusz
@@ -15,110 +33,113 @@ def edit_excel(workbook,i_mama, i_mail, i_r1, i_r2, i_r3):
     hashImieMail = {}
     hashImieIlosc = {}
 
-    try:
-        # =-=-=-=-=-=-=-=-=SORTING DATA FROM OLD WORKBOOK=-=-=-=-=-=-=-=-=
-        for row in sheet.iter_rows(min_row=2,min_col=0, values_only=True):
-            mama = row[i_mama] if row[i_mama] is not None else 'nieznany_rodzic'
-            mail = row[i_mail] if row[i_mail] is not None else ''
-            moneyR1 = row[i_r1] if row[i_r1] is not None else 0
-            moneyR2 = row[i_r2] if row[i_r2] is not None else 0
-            moneyR3 = row[i_r3] if row[i_r3] is not None else 0
-            moneyAll = moneyR1 + moneyR2 + moneyR3
-            if mama in hashImieR1:
-                hashImieR1[mama] += moneyR1
-            else:
-                hashImieR1[mama] = moneyR1
+    # =-=-=-=-=-=-=-=-=SORTING DATA FROM OLD WORKBOOK=-=-=-=-=-=-=-=-=
+    for row in sheet.iter_rows(min_row=2,min_col=0, values_only=True):
+        mama = row[i_mama] if row[i_mama] is not None else 'nieznany_rodzic'
+        mail = row[i_mail] if row[i_mail] is not None else ''
+        moneyR1 = int(row[i_r1]) if row[i_r1] is not None else 0
+        moneyR2 = int(row[i_r2]) if row[i_r2] is not None else 0
+        moneyR3 = int(row[i_r3]) if row[i_r3] is not None else 0
+        moneyAll = moneyR1 + moneyR2 + moneyR3
+        if mama in hashImieR1:
+            hashImieR1[mama] += moneyR1
+        else:
+            hashImieR1[mama] = moneyR1
 
-            if mama in hashImieR2:
-                hashImieR2[mama] += moneyR2
-            else:
-                hashImieR2[mama] = moneyR2
+        if mama in hashImieR2:
+            hashImieR2[mama] += moneyR2
+        else:
+            hashImieR2[mama] = moneyR2
 
-            if mama in hashImieR3:
-                hashImieR3[mama] += moneyR3
-            else:
-                hashImieR3[mama] = moneyR3
+        if mama in hashImieR3:
+            hashImieR3[mama] += moneyR3
+        else:
+            hashImieR3[mama] = moneyR3
 
-            if mama in hashImieALL:
-                hashImieALL[mama] += moneyAll
-            else:
-                hashImieALL[mama] = moneyAll
+        if mama in hashImieALL:
+            hashImieALL[mama] += moneyAll
+        else:
+            hashImieALL[mama] = moneyAll
 
-            if mama in hashImieIlosc:
-                hashImieIlosc[mama] += 1
-            else:
-                hashImieIlosc[mama] = 1
+        if mama in hashImieIlosc:
+            hashImieIlosc[mama] += 1
+        else:
+            hashImieIlosc[mama] = 1
 
-            hashImieMail[mama] = mail
-    except Exception as e:
-        raise ValueError("Zle sformatowane dane/puste wiersze")
+        hashImieMail[mama] = mail
 
-    try:
-        # =-=-=-=-=-=-=-=-=STYLING AND FILLING NEW WORKBOOK=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=STYLING AND FILLING NEW WORKBOOK=-=-=-=-=-=-=-=-=
 
-        # ============Styling============
-        header_font = Font(bold=True)
-        header_alignment = Alignment(horizontal='center', vertical='center')
-        cell_alignment = Alignment(horizontal='left', vertical='center')
-        light_gray_fill = PatternFill(start_color='D3D3D3', end_color='D3D3D3', fill_type='solid')
+    # ============Styling============
+    header_font = Font(bold=True)
+    header_alignment = Alignment(horizontal='center', vertical='center')
+    cell_alignment = Alignment(horizontal='left', vertical='center')
+    light_gray_fill = PatternFill(start_color='D3D3D3', end_color='D3D3D3', fill_type='solid')
 
 
-        # ============Header creation============
-        headers = ['Zwrot','Nazwisko', 'Mail', 'Ilość Dzieci', 'R1', 'R2', 'R3', 'All']
-        for col_num, header in enumerate(headers, start=1):
-            cell = newSheet.cell(row=1, column=col_num, value=header)
-            cell.font = header_font
-            cell.alignment = header_alignment
-            cell.fill = PatternFill(start_color='00969696', end_color='00969696', fill_type='solid')
+    # ============Header creation============
+    headers = ['Zwrot','Nazwisko', 'Mail', 'Ilość Dzieci', 'R1', 'R2', 'R3', 'All']
+    for col_num, header in enumerate(headers, start=1):
+        cell = newSheet.cell(row=1, column=col_num, value=header)
+        cell.font = header_font
+        cell.alignment = header_alignment
+        cell.fill = PatternFill(start_color='00969696', end_color='00969696', fill_type='solid')
 
-        # ============Filling data============
-        row = 2
-        for mama in hashImieMail.keys():
+    # ============Filling data============
+    row = 2
+    for mama in hashImieMail.keys():
+        nazwiskoMama = ""
+
+        # name processing
+        if mode == 0:
+            nazwiskoMama = mama.split()
+            nazwiskoMama = nazwiskoMama[1:] if len(nazwiskoMama) != 1 else nazwiskoMama[0] + " nieznane_nazwisko"
+            nazwiskoMama = ''.join(nazwiskoMama)
+        elif mode == 1:
             nazwiskoMama = mama.split()
             nazwiskoMama = nazwiskoMama[1:] if len(nazwiskoMama) != 1 else nazwiskoMama[0] + " nieznane_nazwisko"
             nazwiskoMama = ''.join(nazwiskoMama)
 
-            mailMama = hashImieMail[mama].split(';')
-            mailMama = '|'.join(mailMama)
+        # mail processing
+        mailMama = hashImieMail[mama].split(';')
+        mailMama = '|'.join(mailMama)
 
-            newSheet.cell(row=row, column=1, value="Szanowna Pani")
-            newSheet.cell(row=row, column=2, value=nazwiskoMama if mama!="nieznany_rodzic" else "Nie znaleziono mamy!")
-            newSheet.cell(row=row, column=3, value= mailMama if mama!="nieznany_rodzic" else "Uzupelnij dane o imie oraz nazwisko mamy")
-            newSheet.cell(row=row, column=4, value=hashImieIlosc[mama])
-            newSheet.cell(row=row, column=5, value=hashImieR1[mama]).number_format = '#,##0.00 "zł"'
-            newSheet.cell(row=row, column=6, value=hashImieR2[mama]).number_format = '#,##0.00 "zł"'
-            newSheet.cell(row=row, column=7, value=hashImieR3[mama]).number_format = '#,##0.00 "zł"'
-            newSheet.cell(row=row, column=8, value=hashImieALL[mama]).number_format = '#,##0.00 "zł"'
-            row += 1
+        newSheet.cell(row=row, column=1, value="Szanowna Pani")
+        newSheet.cell(row=row, column=2, value=nazwiskoMama if mama!="nieznany_rodzic" else "Nie znaleziono mamy!")
+        newSheet.cell(row=row, column=3, value= mailMama if mama!="nieznany_rodzic" else "Uzupelnij dane o imie oraz nazwisko mamy")
+        newSheet.cell(row=row, column=4, value=hashImieIlosc[mama])
+        newSheet.cell(row=row, column=5, value=hashImieR1[mama]).number_format = '#,##0.00 "zł"'
+        newSheet.cell(row=row, column=6, value=hashImieR2[mama]).number_format = '#,##0.00 "zł"'
+        newSheet.cell(row=row, column=7, value=hashImieR3[mama]).number_format = '#,##0.00 "zł"'
+        newSheet.cell(row=row, column=8, value=hashImieALL[mama]).number_format = '#,##0.00 "zł"'
+        row += 1
 
-        # ============Styling============
-        row = 2
-        for mama in hashImieMail.keys():
-            for i in range(1,9):
-                if row % 2 != 0:
-                    newSheet.cell(row=row, column=i).fill = light_gray_fill
-            row += 1
+    # ============Styling============
+    row = 2
+    for mama in hashImieMail.keys():
+        for i in range(1,9):
+            if row % 2 != 0:
+                newSheet.cell(row=row, column=i).fill = light_gray_fill
+        row += 1
 
-        row = 2
-        for mama in hashImieMail.keys():
-            for i in range(1,9):
-                newSheet.cell(row=row, column=i).alignment = cell_alignment
-            row += 1
+    row = 2
+    for mama in hashImieMail.keys():
+        for i in range(1,9):
+            newSheet.cell(row=row, column=i).alignment = cell_alignment
+        row += 1
 
-        # Adjust column widths
-        for col in newSheet.columns:
-            max_length = 0
-            column = col[0].column_letter  # Get the column letter
-            for cell in col:
-                try:
-                    if len(str(cell.value)) > max_length:
-                        max_length = len(cell.value)
-                except Exception:
-                    pass
-            adjusted_width = (max_length + 10)  # Add some extra space
-            newSheet.column_dimensions[column].width = adjusted_width
-    except Exception as e:
-        raise ValueError("Zle sformatowane dane, prawdopodobnie spacja w środku imienia")
+    # Adjust column widths
+    for col in newSheet.columns:
+        max_length = 0
+        column = col[0].column_letter  # Get the column letter
+        for cell in col:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except Exception:
+                pass
+        adjusted_width = (max_length + 10)  # Add some extra space
+        newSheet.column_dimensions[column].width = adjusted_width
     return newWorkBook
 
 if __name__ == "__main__":
